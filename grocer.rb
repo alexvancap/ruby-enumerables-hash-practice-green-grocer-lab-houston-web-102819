31d19bdbf71bb4f025c044ceafc43f9971cdf003
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-14 18:33:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,6 +20,7 @@ def apply_coupons(cart, coupons)
       else
         cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+        puts art["#{name} W/COUPON"]
       end
       cart[name][:count] -= coupon[:num]
     end
