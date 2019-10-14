ad1e919973c398fc8df8aaca9677b54f591a382d
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-14 18:03:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ cart = {
 def apply_clearance(cart)
   cart.each{|item|
     if item[1][:clearance]
-      p (item[1][:price] / 20) * 100
+      p (item[1][:price] / 20) * 10
     end
   }
   return cart
