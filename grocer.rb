45d19671eac5795f383511b4823d3deb92363dbe
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-14 17:51:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,71 @@
 def consolidate_cart(cart)
-  # code here
+  result = {}
+
+  cart.each do |item|
+    item_name = item.keys[0]
+
+    result[item_name] = item.values[0]
+
+    if result[item_name][:count]
+      result[item_name][:count] += 1
+    else
+      result[item_name][:count] = 1
+    end
+  end
+
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  coupons.each do |coupon|
+  coupon_name = coupon[:item]
+  coupon_item_num = coupon[:num]
+  cart_item = cart[coupon_name]
+
+  next if cart_item.nil? || cart_item[:count] < coupon_item_num
+
+  cart_item[:count] -= coupon_item_num
+
+  coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
+  if coupon_in_cart
+    coupon_in_cart[:count] += 1
+  else
+    cart["#{coupon_name} W/COUPON"] = {
+      price: (coupon[:cost] / 2),
+      clearance: cart_item[:clearance],
+      count: 2
+    }
+  end
 end
 
-def apply_clearance(cart)
-  # code here
+cart
 end
+
+
+
+coupons = [
+  {:item => "AVOCADO", :num => 2, :cost => 5.00},
+  {:item => "BEER", :num => 2, :cost => 20.00},
+  {:item => "CHEESE", :num => 3, :cost => 15.00}
+]
+
+cart = {
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 3},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1}
+}
+
+
+def apply_clearance(cart)
+  cart.each{|item|
+    if item[:clearance] == true
+    end
+  }
+  return cart
+end
+
+p apply_clearance(cart)
 
 def checkout(cart, coupons)
   # code here
